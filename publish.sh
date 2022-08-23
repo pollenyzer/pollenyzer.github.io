86359336a5d42b84e297d8f93a8549c5c56441d5
parzival <parzival@trio.li>
2022-08-24 00:44:26 +0200

    publish

---
+++ publish.sh
@@ -0,0 +1,6 @@
+
+cp /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyze/dist/* home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer.github.io/
+cd /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer.github.io
+git add .
+git commit -m "publish"
+git push
