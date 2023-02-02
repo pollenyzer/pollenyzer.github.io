1923d00442c913fea7ccca02dedf28acd16a40dc
parzival <parzival@trio.li>
2023-02-02 09:57:09 +0100

    v

--- publish.sh
+++
@@ -1,8 +0,0 @@
-
-cd /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer/
-npm run build
-cp -r /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer/dist/* /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer.github.io/
-cd /home/bemootzer/promotion/research/projects/pollen_app/app/pollenyzer.github.io/
-git add .
-git commit -m "publish"
-git push
